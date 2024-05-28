ff4501e468ec54e47cf1fdb5d0c5ea3c5d9084d6
“[Muhammad <umer.akramk15@gmail.com>
2024-05-28 14:13:32 +0500

    Created Signup functionality

--- src/helpers/sendVerificationCode.ts
+++ src/helpers/sendVerificationCode.ts
@@ -8,7 +8,7 @@ export async function sendVerificationEmail(
   verifyCode: string
 ): Promise<ApiResponse> {
   try {
-    const { data, error } = await resend.emails.send({
+    await resend.emails.send({
       from: "onboarding@resend.dev",
       to: email,
       subject: "Mystry Message | Verification code",
